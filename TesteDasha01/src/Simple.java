fd09bdb3c6db3c8f22fd7d0daf1d51e041a44b2b
User <User@M6700>
2021-06-23 23:52:09 -0400

    First commit

---
+++ TesteDasha01/src/Simple.java
@@ -0,0 +1,21 @@
+
+class Simple{  
+	
+	
+    public static void main(String args[]){  
+    	
+    	
+     System.out.println("Eu sou Dasha");  
+     
+     int variavel1 = 4;
+     int variavel2 = 5;
+     
+     int sum = 0;
+     
+     sum = variavel1 + variavel2;
+     
+     System.out.println("Valor da soma");  
+     System.out.println(sum);  
+     
+    }  
+}  
